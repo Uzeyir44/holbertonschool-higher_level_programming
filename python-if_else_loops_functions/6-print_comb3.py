7a1c65b021850c7f08a2d853f9cc7002e7852f8c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-16 19:40:32 +0400

    wrong logic

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for i in range(9):
         if (i != 0):
             print(", ", end="")
         
-        if (i != 8 and j != 9):
+        if (i != 8 or j != 9):
             print("{:02}".format(i*10 + j), end="")
         else:
             print("{:02}".format(i*10 + j))
