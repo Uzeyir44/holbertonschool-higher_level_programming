2c4df7837dadfa5c1d15d1c07dc146015f66d7f8
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-16 19:10:46 +0400

    checker

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(100):
-    if (i != 0):
-        print(", ", end="")
-    
-    print("{:02}".format(i), end="")
+for i in range(99):   
+    print("{:02}, ".format(i), end="")
+
+print("99")
