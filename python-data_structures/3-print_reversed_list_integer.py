fc05c049495e7f2b0e599735bd2f9dca9b85ad19
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-19 18:58:20 +0400

    right logic

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, 0, -1):
-        print("{:d}".format(i))
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
+
+a = [1, 2, 3]
+print_reversed_list_integer(a)
