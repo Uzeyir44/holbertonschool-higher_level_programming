4bc9eec94467b20607207523b76c6335a8557a1c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-19 19:00:37 +0400

    forgot to remove a test block

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,3 @@
 def print_reversed_list_integer(my_list=[]):
     for i in range(len(my_list) - 1, -1, -1):
         print("{:d}".format(my_list[i]))
-
-a = [1, 2, 3]
-print_reversed_list_integer(a)
