524267087edf0a1d793a6d1bb7a2895097b6068c
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-19 09:51:40 +0400

    added printig the total number of arguments first

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,5 +2,10 @@
 
 if (__name__ == "__main__"):
     import sys
+    if (len(sys.argv) == 0):
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
