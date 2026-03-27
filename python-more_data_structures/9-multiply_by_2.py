c5bc578e9f1be0514562bb8313895197709b13e2
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-27 19:08:02 +0400

    wrong syntax at line 7

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,6 +4,6 @@ def multiply_by_2(a_dictionary):
     new_dict = {}
 
     for k in a_dictionary:
-        new_dict[k] == a_dictionary[k] * 2
+        new_dict[k] = a_dictionary[k] * 2
 
     return (new_dict)
