00630e4d50c9cb05d77ed6d4461e3fdea59a50a1
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-27 19:57:06 +0400

    pycodestyle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     answ = 0
-    
+
     if (type(roman_string) is not str or roman_string is None):
         return (0)
 
