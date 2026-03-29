511504dc838b29811f3ae14e800646f41252f29a
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-29 14:56:30 +0400

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    Adds two numbers
+    
+    Args:
+        a (int or float): first argument
+        b (int or float): second argument
+    
+    Returns:
+        int: result of addition
+        
+    Raises:
+        TypeError: if a or b are not int or float
+    """
+    try:
+        a = int(a)
+        b = int(b)
+    except:
+        raise TypeError("a must be an integer or b must be an integer")
+
+    return (a + b)
