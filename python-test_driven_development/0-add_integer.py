fb1bf8ff1b600250ee146402f18ee40c3a773401
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-03-29 15:29:09 +0400

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
-This module provides function for addition of two numbers    
+This module provides function for addition of two numbers
 """
 
-def add_integer(a, b=98):
+def add_integer(a, b = 98):
     """
     Adds two numbers
-    
+
     Args:
         a (int or float): first argument
         b (int or float): second argument
-    
+
     Returns:
         int: result of addition
-        
+
     Raises:
         TypeError: if a or b are not int or float
     """
