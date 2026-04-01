2dc4be4a2468f154d33f428c09a18a979d323d05
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-01 22:45:18 +0400

    task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains class Square
+"""
+
+
+class Square(object):
+    """
+    This class represents a square
+
+    Args:
+        object (_type_): superclass with basic commands
+
+    Attributes:
+        No attributes
+    """
+
+    def __init__(self):
+        pass
---
+++ python-classes/README.md
@@ -0,0 +1,7 @@
+##Projects
+
+###0-square.py
+
+Write an empty class Square that defines a square:
+
+-You are not allowed to import any module
