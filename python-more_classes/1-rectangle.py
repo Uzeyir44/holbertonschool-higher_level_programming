5ae3d5df8035dff577347730133fb4bcdf9b259f
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-02 18:42:01 +0400

    task 2

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+This module contains class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    This class represents a rectangle
+
+    Attributes:
+        width (int): private attribute representing the width
+        height (int): private attribute representing the height
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        This functions serves as getter
+
+        Return:
+            self.__width (int): the width of rectangle
+        """
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """
+        This function sets new value for width attribute
+        """
+        if (type(value) is not int):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        This functions serves as getter
+
+        Return:
+            self.__height (int): the width of rectangle
+        """
+        return(self.__height)
+
+    @height.setter
+    def height(self, value):
+        """
+        This function sets new value for height attribute
+        """
+        if (type(value) is not int):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+
+        self.__height = height
+
