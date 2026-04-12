813200427b21884994c9ee73d17f9bf0248f170d
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-12 13:38:42 +0400

    task 0

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This module contains a lookup function
+"""
+
+def lookup(object):
+    """
+    This function prints all attributes of a given class
+
+    Arguments:
+        object (class): a particular class
+
+    Return:
+        (list): returns a list of class attributes
+    """
+    return (dir(object))
---
+++ python-inheritance/README.md
@@ -0,0 +1,8 @@
+##Projects
+
+###0-lookup.py
+
+Write a function that returns the list of available attributes and methods of an object:
+
+- Prototype: def lookup(obj):
+- Returns a list object
