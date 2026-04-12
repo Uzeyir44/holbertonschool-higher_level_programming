a26f6920070c64293519f4dfc5cd26118f928bed
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-12 14:24:48 +0400

    now returns true only if it exact the same class parent classes doesnot work

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -17,4 +17,4 @@ def is_same_class(obj, a_class):
         (bool): True if instance is from this class, False if
         opposite
     """
-    return (isinstance(obj, a_class))
+    return (type(obj) is a_class)
