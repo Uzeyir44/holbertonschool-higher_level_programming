3cbc9a8a57bd25ee4bb6f06507dbe73b41e7c82a
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-15 00:02:40 +0400

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains write_file function
+"""
+
+
+def write_function(filename="", text=""):
+    """
+    This function overwrites file with a new text
+
+    Args:
+        filename (str, optional): file to be ovewritten. Defaults to "".
+        text (str, optional): new text. Defaults to "".
+
+    Returns:
+        int: the number of written characters
+    """
+    with open(filename, mode="w", encoding="utf-8") as afile:
+        afile.write(text)
+        return (len(text))
