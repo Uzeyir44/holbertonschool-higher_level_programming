7890f605e1412b6b3e9ba1cd7023c02dd482aa7b
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-14 23:56:19 +0400

    now function prints the text inside file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,10 +6,10 @@ This module contains read_file function
 
 def read_file(filename=""):
     """
-    This function reads a given file
+    This function reads a given file and prints it
 
     Args:
         filename (str, optional): a path to the file. Defaults to "".
     """
     with open(filename, encoding="utf-8") as afile:
-        afile.read()
+        print(afile.read())
