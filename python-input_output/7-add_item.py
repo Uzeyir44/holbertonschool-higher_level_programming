dcbe3b38edc220ae85595c44d943cae9ffa88ab2
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-15 16:51:23 +0400

    task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains add_item function
+"""
+import sys
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    ls = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    ls = []
+
+for i in range(1, len(sys.argv)):
+    ls.append(sys.argv[i])
+
+save_to_json_file(ls, "add_item.json")
