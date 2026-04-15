9e0809fdfacc9fc0fdf1524542eb418beae3c77d
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-15 14:42:43 +0400

    task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains load_from_json_file function
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    This function converts json file to
+    back to python object
+
+    Args:
+        filename: path to the given file
+    """
+    with open(filename, encoding="utf-8") as json_file:
+        json_string = json_file.read()
+        return (json_string.loads())
