bfe849a877c813e09b9d5af116762c35f6f053dd
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-15 18:01:09 +0400

    task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This module contains Student class
+"""
+
+
+class Student:
+    """
+    This class describes Student
+
+    Attributes:
+        first_name (string): the name of student
+        last_name (string): the last name of student
+        age (int): the age of the student
+
+    Methods:
+        to_json: returns the library of instances attributes
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        This function creates a dictionary of instance's attributes
+
+        Return:
+            (dict): dictionary of the attributes
+        """
+        dct = {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+            }
+        if (attrs == None):
+            return (dct)
+        new_dct = {}
+        for i in attrs:
+            if i in dct.keys():
+                new_dct[i] = dct[i]
+        return (new_dct)
+
+u = Student("Ali", "Abuzarov", 17)
+print(u.to_json(["middle name"]))
