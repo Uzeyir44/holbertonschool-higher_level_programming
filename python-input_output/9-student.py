584607b37a874a095c87f8643bdaf402960656a4
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-15 17:25:10 +0400

    forgot to divide elements of dictionary with comma

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -30,6 +30,6 @@ class Student:
         """
         return {
             "first_name" : self.first_name,
-            "last_name" : self.last_name
+            "last_name" : self.last_name,
             "age" : self.age
         }
