b18efa6b7f9c0255c159263737fe124679f3d501
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-24 19:17:24 +0400

    checker wants other content when undefined endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -45,7 +45,7 @@ class MyHandler(BaseHTTPRequestHandler):
         else:
             self.send_response(404)
             self.end_headers()
-            self.wfile.write(b"404 Not Found")
+            self.wfile.write(b"Not Found")
 
 def run(class_server=HTTPServer, class_handler=MyHandler):
     """
