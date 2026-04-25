5969b4488adf4288ae6c41cae06b1a695ee27540
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-04-25 11:28:00 +0400

    created different behaviour for different endpoints added post functionality

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,11 +1,51 @@
 #!/usr/bin/python3
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 
 @app.route("/")
 def home():
     return ("Welcome to the Flask API!")
+
+@app.route("/data")
+def to_json():
+    usernames = list(users.keys())
+    return (jsonify(usernames))
+
+@app.route("/status")
+def send_ok():
+    return ("OK")
+
+@app.route("/users/<username>")
+def get_user(username):
+    if (username in users):
+        return (jsonify(users[username]))
+    else:
+        return (jsonify({"error": "User not found"}), 404)
+
+@app.route("/add_user", methods=["POST"])
+def new_user():
+    data = request.get_json()
+    
+    if not data:
+        return ({"error":"Invalid JSON"}, 400)
+    
+    username = data.get("username")
+    
+    if not username:
+        return ({"error":"Username is required"}, 400)
+    
+    if username in users:
+        return ({"error":"Username already exists"}, 409)
+    
+    users[username] = data
+    return (jsonify({
+        "message": "User added successfully",
+        "user": data
+    }), 201)
+    
 
 if (__name__ == "__main__"):
     app.run(debug=True)
