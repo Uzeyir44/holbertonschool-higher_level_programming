99af1e35daabd468504a6e1999521cda1c465239
Uzeyir44 <uzeirdzahangirov@gmail.com>
2026-05-13 14:30:43 +0400

    want referencing to ids properly

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-document.querySelector('red_header').addEventListener('click', function() {
-    document.querySelector('header').style.color = '#FF0000';
+document.querySelector('#red_header').addEventListener('click', function() {
+    document.querySelector('#red_header').style.color = '#FF0000';
 });
